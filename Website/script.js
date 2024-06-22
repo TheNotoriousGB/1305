602a68303beab63bb9bc4c2bd511f0a5df237356
Gabriel Antonio Bischof <G.Bischof.inf22@stud.bbbaden.ch>
2024-06-22 13:28:05 +0200

    Design fertiggestellt

--- Website/script.js
+++ Website/script.js
@@ -1,40 +1,41 @@
-
 // Funktion zum Verarbeiten des Formulars
 function handleFormSubmit(event) {
-    event.preventDefault(); // Standard-Formular-Submit verhindern
-    
-    // Formulardaten sammeln
-    const formData = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    };
-    
-    // Daten an die API mit einer POST-Anfrage senden
-    fetch("https://localhost:8443/api/Kundenfeedback", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(formData)
+  event.preventDefault(); // Standard-Formular-Submit verhindern
+
+  // Formulardaten sammeln
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  // Daten an die API mit einer POST-Anfrage senden
+  fetch("https://localhost:8443/api/Kundenfeedback", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+    .then((data) => {
+      console.log("Success:", data);
+      //Erfolgsmeldung an den Benutzer anzeigen
+      alert("Vielen Dank für Ihr Feedback!");
+      // Formular zurücksetzen
+      form.reset();
     })
-    .then(data => {
-        console.log("Success:", data);
-        // Optional: Erfolgsmeldung an den Benutzer anzeigen
-        alert("Vielen Dank für Ihr Feedback!");
-        // Formular zurücksetzen
-        form.reset();
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        // Optional: Fehlermeldung an den Benutzer anzeigen
-        alert("Beim Senden Ihres Feedbacks ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.");
+    .catch((error) => {
+      console.error("Error:", error);
+      // Optional: Fehlermeldung an den Benutzer anzeigen
+      alert(
+        "Beim Senden Ihres Feedbacks ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut."
+      );
     });
 }
 
